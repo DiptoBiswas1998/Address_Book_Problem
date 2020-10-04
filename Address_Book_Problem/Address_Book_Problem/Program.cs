c72331ef4b935af0deaa6c18f3adc03dac533071
Dipto <diptobiswas1998@gmail.com>
2020-10-05 01:59:42 +0530

    [Dipto] refactor: Adding a contact

--- Address_Book_Problem/Address_Book_Problem/Program.cs
+++ Address_Book_Problem/Address_Book_Problem/Program.cs
@@ -7,6 +7,9 @@ namespace Address_Book_Problem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Address Book Problem!");
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact("Dipto", "Biswas", "Rahara, Khardah", "Kolkata", "West Bengal", "700118", "9876543210", "diptobiswas@gmail.com");
+            addressBook.ViewContact();
         }
     }
 }
