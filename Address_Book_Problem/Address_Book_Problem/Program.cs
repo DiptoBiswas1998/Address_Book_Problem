af2d39dd4a5b186754dbb9feb5e246c459ede9a5
Dipto <diptobiswas1998@gmail.com>
2020-10-05 01:23:14 +0530

    [Dipto] refactor: Welcome msg

---
+++ Address_Book_Problem/Address_Book_Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Address_Book_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book Problem!");
+        }
+    }
+}
