126094025376cc65930462be4b46d57c9c3e1762
Dipto <diptobiswas1998@gmail.com>
2020-10-05 02:05:35 +0530

    [Dipto] refactor: Taking Contact as an Input

--- Address_Book_Problem/Address_Book_Problem/Program.cs
+++ Address_Book_Problem/Address_Book_Problem/Program.cs
@@ -6,9 +6,25 @@ namespace Address_Book_Problem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Address Book Problem!");
+            Console.WriteLine("Welcome to Address Book Program.");
             AddressBook addressBook = new AddressBook();
-            addressBook.AddContact("Dipto", "Biswas", "Rahara, Khardah", "Kolkata", "West Bengal", "700118", "9876543210", "diptobiswas@gmail.com");
+            Console.WriteLine("Enter first Name: ");
+            String firstName = Console.ReadLine();
+            Console.WriteLine("Enter last Name: ");
+            String lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address: ");
+            String address = Console.ReadLine();
+            Console.WriteLine("Enter city: ");
+            String city = Console.ReadLine();
+            Console.WriteLine("Enter state: ");
+            String state = Console.ReadLine();
+            Console.WriteLine("Enter zip: ");
+            String zip = Console.ReadLine();
+            Console.WriteLine("Enter phNo: ");
+            String phNo = Console.ReadLine();
+            Console.WriteLine("Enter email: ");
+            String email = Console.ReadLine();
+            addressBook.AddContact(firstName, lastName, address, city, state, zip, phNo, email);
             addressBook.ViewContact();
         }
     }
