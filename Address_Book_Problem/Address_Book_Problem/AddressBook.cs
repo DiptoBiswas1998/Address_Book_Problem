7d57a67f286fe0ced8d3799d19d5cda21b550faa
unknown <diptobiswas1998@gmail.com>
2020-10-29 08:58:20 +0530

    [Dipto] refactor: Modifying the code to add multiple Address Books

--- Address_Book_Problem/Address_Book_Problem/AddressBook.cs
+++ Address_Book_Problem/Address_Book_Problem/AddressBook.cs
@@ -6,6 +6,7 @@ namespace Address_Book_Problem
     class AddressBook
     {
         private Dictionary<string, Contact> addressBook = new Dictionary<string, Contact>();
+        private Dictionary<string, AddressBook> addressBookDictionary = new Dictionary<string, AddressBook>();
         public void AddContact(string firstName, string lastName, string address, string city, string state, string email, string zip, string phoneNumber)
         {
             Contact contact = new Contact();
@@ -109,6 +110,16 @@ namespace Address_Book_Problem
             {
                 Console.WriteLine("\nNot Found, Try Again.\n");
             }
+        }
+        public void AddAddressBook(string bookName)
+        {
+            AddressBook addressBook = new AddressBook();
+            addressBookDictionary.Add(bookName, addressBook);
+            Console.WriteLine("AddressBook Created.");
+        }
+        public Dictionary<string, AddressBook> GetAddressBook()
+        {
+            return addressBookDictionary;
         }
     }
 }
